4ea04f1b35ec6fb4e835cc1009f414a03a2dc972
joe beuckman <joebeuckman@gmail.com>
2014-01-05 18:13:56 -0600

    saving and loading outlines

--- src/ofxFeatureFinderObject.cpp
+++ src/ofxFeatureFinderObject.cpp
@@ -31,14 +31,35 @@ void ofxFeatureFinderObject::save(string filepath)
         cout << "ERROR OPENING FILE TO WRITE" << endl;
         return;
     }
-    cout << "now saving " << keypoints.size() << " keypoints..." << endl;
 
-    fs << "keypoints" << "[";
+    cout << "now saving " << outlines.size() << " outlines..." << endl;
     
+    fs << "outlines" << "[";
+    for(unsigned int j=0; j<outlines.size(); ++j)
+    {
+        vector<ofPoint> vertices = outlines.at(j).getVertices();
+        
+        fs << "[";
+        for(unsigned int i=0; i<vertices.size(); ++i)
+        {
+            fs << "{" <<
+            "x" << vertices.at(i).x <<
+            "y" << vertices.at(i).y <<
+            "}";
+        }
+        fs << "]";
+        
+    }
+    fs << "]";
+    
+    
+    cout << "now saving " << keypoints.size() << " keypoints..." << endl;
+    
+    fs << "keypoints" << "[";
     for(unsigned int j=0; j<keypoints.size(); ++j)
     {
         cv::KeyPoint kp = keypoints.at(j);
-        
+            
         fs << "{" <<
         "angle" << kp.angle <<
         "class_id" << kp.class_id <<
@@ -49,14 +70,13 @@ void ofxFeatureFinderObject::save(string filepath)
         "size" << kp.size <<
         "}";
     }
-    
     fs << "]";
 
     cout << "now saving descriptors..." << endl;
     fs << "descriptors" << descriptors;
 
     fs.release();
-    cout << "saving complete" << endl;
+    cout << "ofxFeatureFinderObject::save complete" << endl;
     
 }
 
@@ -69,23 +89,42 @@ void ofxFeatureFinderObject::load(string filepath)
         cout << "ERROR OPENING FILE TO READ" << endl;
         return;
     }
-
-    cv::FileNode keypointsList = fs["keypoints"];
-    cv::FileNodeIterator it = keypointsList.begin(), it_end = keypointsList.end();
+    
+    cv::FileNode outlinesList = fs["outlines"];
+    cv::FileNodeIterator it = outlinesList.begin(), it_end = outlinesList.end();
     int idx = 0;
-    std::vector<uchar> lbpval;
     
     for( ; it != it_end; ++it, idx++ )
     {
-        cv::KeyPoint kp;
+        ofPolyline line;
+        
+        for (int k=0; k<(*it).size(); k++) {
+            ofPoint p = ofPoint((*it)[k]["x"], (*it)[k]["y"]);
+            line.addVertex(p);
+        }
+        
+        line.close();
+        
+        outlines.push_back(line);
+    }
+    cout << "loaded " << outlines.size() << " outlines." << endl;
+    
+    
+    cv::FileNode keypointsList = fs["keypoints"];
+    cv::FileNodeIterator it2 = keypointsList.begin(), it2_end = keypointsList.end();
+    idx = 0;
 
-        kp.angle = (*it)["angle"];
-        kp.class_id = (*it)["class_id"];
-        kp.octave = (*it)["octave"];
-        kp.pt.x = (*it)["x"];
-        kp.pt.y = (*it)["y"];
-        kp.response = (*it)["response"];
-        kp.size = (*it)["size"];
+    for( ; it2 != it2_end; ++it2, idx++ )
+    {
+        cv::KeyPoint kp;
+            
+        kp.angle = (*it2)["angle"];
+        kp.class_id = (*it2)["class_id"];
+        kp.octave = (*it2)["octave"];
+        kp.pt.x = (*it2)["x"];
+        kp.pt.y = (*it2)["y"];
+        kp.response = (*it2)["response"];
+        kp.size = (*it2)["size"];
         
         keypoints.push_back(kp);
     }
