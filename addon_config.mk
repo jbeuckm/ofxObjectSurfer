d7b3627b0675b25aacb1737f05f1cd16a5d75c42
joe <joseph.beuckman@usbank.com>
2014-01-14 12:16:50 -0600

    config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+        ADDON_NAME = ofxObjectSurfer
+        ADDON_DESCRIPTION = Preprocessing, feature selection and descriptor file i/o for SURF object detection
+        ADDON_AUTHOR = Joe Beuckman
+        ADDON_TAGS = "computer vision" "machine learning"
+        ADDON_URL = https://github.com/jbeuckm/ofxObjectSurfer
